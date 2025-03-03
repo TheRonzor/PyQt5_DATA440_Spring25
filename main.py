cae232b3c0a87e8cc549243e22f5623f8a227c1b
TheRonzor <ronniecougar@gmail.com>
2025-03-03 13:49:48 -0500

    say hello app done

---
+++ main.py
@@ -0,0 +1,6 @@
+# Ron: put this on Blackboard: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QApplication.html
+
+from src.qtcomponents import SayHelloApp
+
+if __name__ == "__main__":
+    SayHelloApp()
